2b5d09fe78cdfbf6133316df9d3c54c3d92e10fe
yuwei2010 <yuwei2005@gmail.com>
2024-04-17 10:52:10 +0200

    modified:   djsurfer/datainterface.py 	modified:   djsurfer/lib_interface/text_object.py

--- djsurfer/datainterface.py
+++ djsurfer/datainterface.py
@@ -2,17 +2,35 @@
 from abc import ABC, abstractmethod
 
 #%%
+from abc import ABC, abstractmethod
+
 class DataInterface(ABC):
-    
+    """
+    Abstract base class for data interfaces.
+    """
+
     def __init__(self, path, name=None, comment=None, config=None):
-        
+        """
+        Initializes a DataInterface object.
+
+        Parameters:
+        - path (str): The path to the data.
+        - name (str, optional): The name of the data interface. Defaults to None.
+        - comment (str, optional): Any additional comment or description. Defaults to None.
+        - config (dict, optional): Configuration parameters for the data interface. Defaults to None.
+        """
         self.path = path
         self.name = name
         self.comment = comment
         self.config = config
         self.df = self.get_df()
-        
-        
+
     @abstractmethod
     def get_df(self):
+        """
+        Abstract method to get the data as a pandas DataFrame.
+
+        Returns:
+        - df (pandas.DataFrame): The data as a pandas DataFrame.
+        """
         pass
--- djsurfer/lib_interface/text_object.py
+++ djsurfer/lib_interface/text_object.py
@@ -4,18 +4,28 @@ from djsurfer.datainterface import DataInterface
 
 #%%
 class TEXT_OBJECT(DataInterface):
-    
+    """
+    A class representing a text object.
+
+    Args:
+        path (str): The path to the text file.
+        name (str, optional): The name of the text object. Defaults to None.
+        comment (str, optional): Any additional comment about the text object. Defaults to None.
+    """
+
     def __init__(self, path, name=None, comment=None):
-        
         super().__init__(path, name, comment)
         
     def get_df(self):
-        
-        # Read the text file
+        """
+        Read the text file and return its contents as a pandas DataFrame.
+
+        Returns:
+            pandas.DataFrame: The contents of the text file as a DataFrame.
+        """
         with open(self.path, 'r') as f:
             data = f.readlines()
         
-        # Create a dataframe
         df = pd.DataFrame([l.strip().split(',') for l in data[1:]], columns=data[0].strip().split(','))
         
         return df
